6da48957abbe18cee1b420826952f52b808d4a63
Maria Freitas <mariamrfreitas@hotmail.com>
2020-07-09 17:34:21 +0100

    Version with transition to sql working and clean tables

--- Week2_project.py
+++ Week2_project.py
@@ -6,7 +6,7 @@ Created on Thu Jul  9 11:29:36 2020
 """
 
 
-schools = {   
+school_dic = {   
 'ironhack' : 10828,
 'app-academy' : 10525,
 'springboard' : 11035    
@@ -39,7 +39,7 @@ def get_comments_school(school):
 # could you write this as a list comprehension? ;)
 comments = []
 
-for school in schools.keys():
+for school in school_dic.keys():
     print(school)
     comments.append(get_comments_school(school))
 
@@ -82,8 +82,7 @@ courses_list = []
 badges_list = []
 schools_list = []
 
-for school, id in schools.items():
-    print(school)
+for school, id in school_dic.items():
     a,b,c,d = get_school_info(school,id)
     locations_list.append(a)
     courses_list.append(b)
@@ -91,45 +90,85 @@ for school, id in schools.items():
     schools_list.append(d)
     
 locations = pd.concat(locations_list)
-
 courses = pd.concat(courses_list)
-
 badges = pd.concat(badges_list)
-
 schools = pd.concat(schools_list)
+
+#cleaning comments
+def clean_comments(comments):
+    #Changes in types
+    comments['overallScore'] = comments['overallScore'].astype('float')
+    comments['curriculum'] = comments['curriculum'].astype('float')
+    comments['jobSupport'] = comments['jobSupport'].astype('float')
+    comments['id'] = comments['id'].astype('object')
+    comments['queryDate'] = pd.to_datetime(comments['queryDate'])
+    #Change type and clean graduating years
+    def correct_year(x):
+        YEAR_RE = re.compile(r'\..')
+        return YEAR_RE.sub('',x)
+    import datetime
+    comments["graduatingYear"] = comments["graduatingYear"].astype(str).apply(correct_year)
+    #Dropping unecessary columns
+    drop_cols = ["user", "body", "createdAt", "comments", "hostProgramName"]   
+    comments = comments.drop(drop_cols, axis=1)
+    #Renaming columns
+    comments.rename(columns={'review_body': 'Review'}, inplace=True)
+    #Substituting nulls
+    comments = comments.fillna('not available')
+    return comments
+
+#cleaning locations
+def clean_locations(locations):
+    locations['id'] = locations['id'].astype('object')
+    locations['city.id'] = locations['city.id'].astype('object')
+    locations['country.id'] = locations['country.id'].astype('object')
+    locations['state.id'] = locations['state.id'].astype('object')
+    locations['city.id'] = locations['city.id'].astype('object')
+    locations = locations.fillna('not available')
+    return locations
+
+#cleaning badges
+def clean_badge_school(badges):
+    TAG_RE = re.compile(r'<[^>]+>')
+    def remove_tags(x):
+        return TAG_RE.sub('',x)
+    badges['description'] = badges['description'].apply(remove_tags)
+    return badges
+
+#Getting new versions goinggg
+comments = clean_comments(comments) 
+locations = clean_locations(locations)
+badges = clean_badge_school(badges)
+schools = clean_badge_school(schools)
+
+
+#Cretion of table with relation between scholls and id
+id_df=pd.DataFrame(school_dic.items(), columns=["school","school_id"])
+
+#Adding the id of schools to evey column
+locations = locations.merge( id_df, how="inner", left_on='school', right_on='school')
+comments = comments.merge( id_df, how="inner", left_on='school', right_on='school')
+badges = badges.merge( id_df, how="inner", left_on='school', right_on='school')
+schools = schools.merge( id_df, how="inner", left_on='school', right_on='school')
+courses = courses.merge( id_df, how="inner", left_on='school', right_on='school')
+
+
+
+print(locations,comments,badges,schools,courses)
+
 
 from sqlalchemy import create_engine
 
-comments = comments.drop('comments', axis = 1)
-
-comments  = comments.drop('user', axis = 1)
-
-engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}".format(user="root",pw="Miami66!",db="week2_amazingteam"))
+engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}".format(user="root",pw="D4taR00t!",db="amazing_team"))
 schools.to_sql('Schools', con = engine, if_exists = 'append', chunksize = 1000)
-
-
 
 comments.to_sql('Comments', con = engine, if_exists = 'append', chunksize = 1000)
 
-
 badges.to_sql('Badges', con = engine, if_exists = 'append', chunksize = 1000)
-
 
 locations.to_sql('Location', con = engine, if_exists = 'append', chunksize = 1000)
 
-
 courses.to_sql('Courses', con = engine, if_exists = 'append', chunksize = 1000)
-
-
-
-
-
-
-
-
-
-
-
 
 
 
